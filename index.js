ee06d767dfcc552b88e1403fe83c32952cc9d29d
Tabish123t <tabishyunus10@gmail.com>
2024-09-07 14:55:43 +0500

    project_00-Calulator

---
+++ index.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "First_Number",
+        message: "Please Enter Your First Number:"
+    },
+    {
+        type: "number",
+        name: "Second_Number",
+        message: "Please Enter Your Second Number:"
+    },
+    {
+        type: "list",
+        name: "Operator",
+        message: "Select an operator:",
+        choices: ["+", "-", "*", "/"]
+    }
+]);
+let result;
+switch (answer.Operator) {
+    case "+":
+        result = answer.First_Number + answer.Second_Number;
+        break;
+    case "-":
+        result = answer.First_Number - answer.Second_Number;
+        break;
+    case "*":
+        result = answer.First_Number * answer.Second_Number;
+        break;
+    case "/":
+        result = answer.First_Number / answer.Second_Number;
+        break;
+    default:
+        result = "Invalid operator selected";
+}
+console.log("Result: ", result);
